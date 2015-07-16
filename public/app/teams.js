93a2ffa10b48ac48b4e313986467b3b66e156503
Bernardo Zuniga <bzuniga@mdsol.com>
2015-07-16 16:08:08 -0400

    remove host form api url

--- public/app/teams.js
+++ public/app/teams.js
@@ -1,6 +1,6 @@
 angular.module('n15-teams', ['ngResource'])
   .factory('TeamService', ['$resource', function($resource) {
-      return $resource('http://localhost:5000/api/teams', {team: '@team'});
+      return $resource('/api/teams', {team: '@team'});
     }])
   .controller('teamController', ['TeamService', '$scope', function(TeamService, $scope) {
     $scope.teams = TeamService.query();
