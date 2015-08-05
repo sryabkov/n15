19dfc8c4ff7d751fedaec367a5b9385cfd9e6e2c
Bernardo Zuniga <bzuniga@mdsol.com>
2015-08-05 10:13:34 -0400

    delete old files

--- public/app/teams.js
+++
@@ -1,7 +0,0 @@
-angular.module('n15-teams', ['ngResource'])
-  .factory('TeamsService', ['$resource', function($resource) {
-      return $resource('/api/teams', {team: '@team'});
-    }])
-  .controller('teamController', ['TeamsService', '$scope', function(TeamsService, $scope) {
-    $scope.teams = TeamsService.query();
-  }]);
